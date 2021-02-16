10d18e7a05e2a6ba9b986921dbf731642fb26192
arvapu <arvydas.apulskis123@gmail.com>
2021-02-16 22:34:58 +0200

    installed cors handling support package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -61,6 +61,7 @@ $app->singleton(
 $app->configure('app');
 $app->configure('mail');
 $app->configure('jwt');
+$app->configure('cors');
 
 /*
 |--------------------------------------------------------------------------
@@ -73,12 +74,12 @@ $app->configure('jwt');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    \Fruitcake\Cors\HandleCors::class,
+]);
 
 $app->routeMiddleware([
- 'auth' => App\Http\Middleware\Authenticate::class,
+    'auth' => App\Http\Middleware\Authenticate::class,
 ]);
 
 /*
@@ -95,6 +96,7 @@ $app->routeMiddleware([
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Illuminate\Mail\MailServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
