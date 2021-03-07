290c90a519b984666bf20e982304d2b0721ceb58
arvapu <arvydas.apulskis123@gmail.com>
2021-03-07 19:59:29 +0200

    added initial admin user migration

---
+++ database/migrations/2021_02_10_195555_add_initial_admin_user_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AddInitialAdminUserToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->insert([
+            'role' => 'admin',
+            'first_name' => 'Admin',
+            'last_name' => 'User',
+            'email' => env('ADMIN_USER_EMAIL', 'admin@fake.mail'),
+            'password' => Hash::make(env('ADMIN_USER_PASSWORD', 'admin')),
+            'email_verified_at' => Carbon::now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('users')->where(
+            'email','=',env('ADMIN_USER_EMAIL', 'admin@fake.mail')
+        )->delete();
+    }
+}
